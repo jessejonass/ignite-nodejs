a0179f1f8590e6a03257493f605972ceeaaed5f4
Jesse Jonas <jessejonas13@gmail.com>
2022-01-20 14:31:42 -0300

    feat: added http routes

--- 1-fundamentals/src/index.js
+++ 1-fundamentals/src/index.js
@@ -1,10 +1,95 @@
 const express = require('express');
 
 const app = express();
+const PORT = 3333;
 
-app.get('/', (request, response) => {
-  return response.send({ ok: true });
+app.get('/courses', (request, response) => {
+  const data = [
+    {
+      id: 1,
+      name: 'ReactJS',
+    },
+    {
+      id: 2,
+      name: 'PHP',
+    },
+  ];
+
+  return response.json(data);
 });
 
-// http://localhost:PORT
-app.listen(3333);
+app.post('/courses', (request, response) => {
+  const data = [
+    {
+      id: 1,
+      name: 'ReactJS',
+    },
+    {
+      id: 2,
+      name: 'PHP',
+    },
+    {
+      id: 3,
+      name: 'Haskell',
+    },
+  ];
+
+  return response.json(data);
+});
+
+app.put('/courses/:id', (request, response) => {
+  const data = [
+    {
+      id: 1,
+      name: 'ReactJS',
+    },
+    {
+      id: 2,
+      name: 'PHP',
+    },
+    {
+      id: 3,
+      name: 'Pearl',
+    },
+  ];
+
+  return response.json(data);
+});
+
+app.patch('/courses/:id', (request, response) => {
+  const data = [
+    {
+      id: 1,
+      name: 'ReactJS',
+    },
+    {
+      id: 2,
+      name: 'PHP',
+    },
+    {
+      id: 3,
+      name: 'Perl',
+    },
+  ];
+
+  return response.json(data);
+});
+
+app.delete('/courses/:id', (request, response) => {
+  const data = [
+    {
+      id: 1,
+      name: 'ReactJS',
+    },
+    {
+      id: 2,
+      name: 'PHP',
+    },
+  ];
+
+  return response.json(data);
+});
+
+app.listen(PORT, () => {
+  console.log(`\u001b[36m| server running on port: ${PORT} |`);
+});
