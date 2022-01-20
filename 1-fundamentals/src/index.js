6aae92210d79cae83e1620f2a186f16546c3f567
Jesse Jonas <jessejonas13@gmail.com>
2022-01-20 14:16:37 -0300

    chore: added nodemon

--- 1-fundamentals/src/index.js
+++ 1-fundamentals/src/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (request, response) => {
-  return response.json({ ok: true });
+  return response.send({ ok: true });
 });
 
 // http://localhost:PORT
