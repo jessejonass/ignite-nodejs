558a2954ecd4ba7bba6fc646ddf7bed5507f43b4
Jesse Jonas <jessejonas13@gmail.com>
2022-03-08 08:49:48 -0300

    feat: added accounting creation

--- 1-project-one/finapi/src/index.js
+++ 1-project-one/finapi/src/index.js
@@ -1,9 +1,32 @@
+const PORT = 3333;
 const express = require('express');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 app.use(express.json());
 
-const PORT = 3333;
+const customers = [];
+
+/**
+ * id: uuid;
+ * cpf: string;
+ * name: string;
+ * statement: [];
+ */
+app.post('/account', (request, response) => {
+  // don't create with existing cpf
+  const { cpf, name } = request.body;
+  const id = uuidv4();
+
+  customers.push({
+    id,
+    cpf,
+    name,
+    statement: [],
+  });
+
+  return response.status(201).send(customers);
+});
 
 app.listen(PORT, () => {
   console.log(`\u001b[36m| server running on port: ${PORT} |`);
