5a3b1660769a5e3f992d6c2adbbd391eb104c17d
Jesse Jonas <jessejonas13@gmail.com>
2022-03-08 09:25:34 -0300

    feat: added get statement route and middleware to get user

--- 1-project-one/finapi/src/index.js
+++ 1-project-one/finapi/src/index.js
@@ -4,28 +4,51 @@ const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 app.use(express.json());
+// app.use(middleware); // use middleware to all routes
 
 const customers = [];
 
-/**
- * id: uuid;
- * cpf: string;
- * name: string;
- * statement: [];
- */
+// middlware
+function verifyIfExistsAccountCpf(request, response, next) {
+  const { cpf } = request.headers;
+
+  const customer = customers.find((customer) => customer.cpf === cpf);
+
+  if (!customer) {
+    return response.status(400).json({ Error: 'Customer not found' });
+  }
+
+  request.customer = customer; // get in routes
+  return next();
+}
+
+// create account
 app.post('/account', (request, response) => {
-  // don't create with existing cpf
   const { cpf, name } = request.body;
-  const id = uuidv4();
+
+  // some cliente with this cpf => return boolean
+  const customerAlreadyExists = customers.some(
+    (customer) => customer.cpf === cpf
+  );
+
+  if (customerAlreadyExists)
+    return response.status(400).json({ Error: 'Customer already exists' });
 
   customers.push({
-    id,
+    id: uuidv4(),
     cpf,
     name,
     statement: [],
   });
 
-  return response.status(201).send(customers);
+  return response.status(201).json(customers);
+});
+
+// get customer statement by cpf
+app.get('/statement', verifyIfExistsAccountCpf, (request, response) => {
+  const { customer } = request; // customer from middleware
+
+  return response.json(customer.statement);
 });
 
 app.listen(PORT, () => {
