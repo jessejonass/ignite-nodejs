0ce9324da4d64e117741dd0525e28efd2ba9c976
Jesse Jonas <jessejonas13@gmail.com>
2022-03-14 20:39:56 -0300

    feat: initialized categories route

---
+++ 2-api-typescript/rentalx/src/routes/categories.routes.ts
@@ -0,0 +1,19 @@
+import { Router } from 'express';
+import { v4 as uuidv4 } from 'uuid';
+
+const categoriesRoutes = Router();
+
+const categories = [];
+
+// categories/
+categoriesRoutes.post('/', (request, response) => {
+  const { name, description } = request.body;
+
+  const category = { id: uuidv4(), name, description };
+
+  categories.push(category);
+
+  return response.status(201).json({ categories });
+});
+
+export { categoriesRoutes };
--- 2-api-typescript/rentalx/src/server.ts
+++ 2-api-typescript/rentalx/src/server.ts
@@ -1,9 +1,9 @@
 import express from 'express';
+import { categoriesRoutes } from './routes/categories.routes';
 
 const app = express();
 
-app.get('/', (request, response) => {
-  return response.json({ ok: true });
-});
+app.use(express.json());
+app.use('/categories', categoriesRoutes); // use route from routes
 
 app.listen(3333, () => console.log('server is running'));
