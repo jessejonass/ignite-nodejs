be8a0e22a86ab883d1163e99583f9ade7d590e60
Jesse Jonas <jessejonas13@gmail.com>
2022-03-22 10:37:50 -0300

    feat: added categories routes

--- 2-api-typescript/rentalx/src/routes/categories.routes.ts
+++ 2-api-typescript/rentalx/src/routes/categories.routes.ts
@@ -1,26 +1,17 @@
 import { Router } from 'express';
-import { CategoriesRepository } from '../modules/cars/repositories/CategoriesRepository';
-import { CreateCategoryService } from '../modules/cars/services/CreateCategoryService';
+import { createCategoryController } from '../modules/cars/useCases/createCategory';
+import { listCategoriesController } from '../modules/cars/useCases/listCategories';
 
 const categoriesRoutes = Router(); // using /categories from server.ts
-const categoriesRepository = new CategoriesRepository(); // access, db, etc...
 
 // categories/
 categoriesRoutes.get('/', (request, response) => {
-  const categories = categoriesRepository.list(); // db accesses, etc...
-
-  return response.json({ categories });
+  return listCategoriesController.handle(request, response);
 });
 
 // categories/
 categoriesRoutes.post('/', (request, response) => {
-  const { name, description } = request.body;
-
-  // using single responsability principle
-  const createCategoryService = new CreateCategoryService(categoriesRepository);
-  createCategoryService.execute({ name, description });
-
-  return response.status(201).send();
+  return createCategoryController.handle(request, response);
 });
 
 export { categoriesRoutes };
