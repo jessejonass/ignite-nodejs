99700b807e7d671efb72008054ed2fdb7fac12f8
Jesse Jonas <jessejonas13@gmail.com>
2022-05-11 09:01:08 -0300

    fix: renamed models dir to entities

--- 2-api-typescript/rentx/src/modules/cars/entities/Category.ts
+++ 2-api-typescript/rentx/src/modules/cars/entities/Category.ts
@@ -1,13 +1,21 @@
 import { v4 as uuidv4 } from 'uuid';
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from 'typeorm';
 
+@Entity('categories')
 class Category {
+  @PrimaryColumn()
   id?: string;
+
+  @Column()
   name: string;
+
+  @Column()
   description: string;
+
+  @CreateDateColumn()
   created_at: Date;
 
   constructor() {
-    // create ID if it doesn't exist
     if (!this.id) {
       this.id = uuidv4();
     }
